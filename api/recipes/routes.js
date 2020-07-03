06d27d7d0fa90eb49f48fa9edb126f7e7b495a8e
unknown <krzlepianka@gmail.com>
2020-07-03 22:39:07 +0200

    refactor receipe Schema, setup multer

--- api/recipes/routes.js
+++ api/recipes/routes.js
@@ -1,10 +1,13 @@
 const controller = require('./controller');
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 router
     .route('/')
     .get(controller.getAllReceipe)
-    .post(controller.createReceipe)
+    .post(upload.single('image'), controller.createReceipe)
+
 
 module.exports = router;
